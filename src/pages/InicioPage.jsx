3614b81d9c048259dfd0e710add7e0fb7cbdf10c
toledanogus <toledanogu@gmail.com>
2024-04-01 23:16:58 -0600

    Diseño no tan mal

--- src/pages/InicioPage.jsx
+++ src/pages/InicioPage.jsx
@@ -9,7 +9,6 @@ export const InicioPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-
   //Funciones***********************************************
   const aNuevaTarea = () => {
     navigate("/nueva");
@@ -17,70 +16,80 @@ export const InicioPage = () => {
 
   const aListaDeTareas = (nuevoFiltro) => {
     dispatch(setFiltro(nuevoFiltro));
-    navigate('/lista');
+    navigate("/lista");
   };
 
   //Efectos*************************************************
 
   return (
     <>
-      <h1>InicioPage</h1>
-      <button
-        onClick={() => {
-          const nuevoFiltro = 'todas'
-          
-          aListaDeTareas(nuevoFiltro);
-        }}
-      >
-        Todas las Tareas
-      </button>
-      <br />
-      <br />
-      <button
-        onClick={() => {
-          const nuevoFiltro = 'escuela'
-          
-          aListaDeTareas(nuevoFiltro);
-        }}
-      >
-        Escuela
-      </button>
-      <button
-        onClick={() => {
-          const nuevoFiltro = 'casa'
-         
-          aListaDeTareas(nuevoFiltro);
-        }}
-      >
-        Casa
-      </button>
-      <button
-        onClick={() => {
-          const nuevoFiltro = 'personal'
-          
-          aListaDeTareas(nuevoFiltro);
-        }}
-      >
-        Personal
-      </button>
-      <button
-        onClick={() => {
-          const nuevoFiltro = 'salud'
-          
-          aListaDeTareas(nuevoFiltro);
-        }}
-      >
-        Salud
-      </button>
-      <button
-        onClick={() => {
-          const nuevoFiltro = 'diversion'
-          
-          aListaDeTareas(nuevoFiltro);
-        }}
-      >
-        Diversión
-      </button>
+      <h1>Gestión de Pendientes</h1>
+      <div className="containerInicio">
+        <div className="todas" colSpan="4">
+          <span
+            onClick={() => {
+              const nuevoFiltro = "todas";
+              aListaDeTareas(nuevoFiltro);
+            }}
+          >
+            Todas las Tareas
+          </span>
+        </div>
+        <div className="escuela" colSpan="4">
+          <span
+            onClick={() => {
+              const nuevoFiltro = "escuela";
+              aListaDeTareas(nuevoFiltro);
+            }}
+          >
+            Escuela
+          </span>
+        </div>
+        <div className="casa" colSpan="4">
+          <span
+            onClick={() => {
+              const nuevoFiltro = "casa";
+              aListaDeTareas(nuevoFiltro);
+            }}
+          >
+            Casa
+          </span>
+        </div>
+      </div>
+{/* linea siguiente *************************************/}
+      <div className="containerInicio">
+      <div className="personal" colSpan="4">
+        <button
+          onClick={() => {
+            const nuevoFiltro = "personal";
+            aListaDeTareas(nuevoFiltro);
+          }}
+        >
+          Personal
+        </button>
+      </div>
+      
+        <div className="salud">
+          <button
+            onClick={() => {
+              const nuevoFiltro = "salud";
+              aListaDeTareas(nuevoFiltro);
+            }}
+          >
+            Salud
+          </button>
+        </div>
+        <div className="diversion">
+          <button
+            onClick={() => {
+              const nuevoFiltro = "diversion";
+              aListaDeTareas(nuevoFiltro);
+            }}
+          >
+            Diversión
+          </button>
+        </div>
+      </div>
       <br />
       <br />
       <button onClick={aNuevaTarea}>Nueva Tarea</button>
